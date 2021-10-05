e5c1463c09404a15ce7bb2ab54ac18b3ab6ac43e
unknown <tucker.serwadda@xfact.com>
2021-10-05 11:18:46 -0400

    added the sorting attribut to the columns and the export button

--- vuex-pagination-app/src/components/EventDisplay.vue
+++ vuex-pagination-app/src/components/EventDisplay.vue
@@ -1,6 +1,7 @@
 <template>
+<div><h2>The Good time events </h2></div>
 	<div>
-        <DataTable :value="events" :paginator="true" :rows="10"
+        <DataTable :value="events" :paginator="true" :rows="10" ref="dt"
             paginatorTemplate="CurrentPageReport FirstPageLink PrevPageLink PageLinks NextPageLink LastPageLink RowsPerPageDropdown"
             :rowsPerPageOptions="[1,2,3]" responsiveLayout="scroll"
             currentPageReportTemplate="Showing {first} to {last} of {totalRecords}"
@@ -8,6 +9,11 @@
                 :globalFilterFields="['title','category','organizer','description']">
                 <!-- add the global filter -->
                 <template #header>
+                    <!-- add an export button -->
+                    <div style="text-align: left">
+                    <Button icon="pi pi-external-link" label="Export" @click="exportCSV($event)" />
+                </div>
+                <!-- add a clear button.. to clear the input text -->
                     <div class="p-d-flex p-jc-between">
                         <Button type="button" icon="pi pi-filter-slash" label="Clear" class="p-button-outlined" @click="clearEventFilters()"/>
                         <span class="p-input-icon-left">
@@ -15,6 +21,7 @@
                             <InputText v-model="eventFilters['global'].value" placeholder="Keyword Search" />
                         </span>
                     </div>
+                    <!-- template to display if no event contains  -->
                 </template>
                             <template #empty>
                     No customers found.
@@ -22,12 +29,12 @@
                 <template #loading>
                     Loading customers data. Please wait.
                 </template>
-            <Column field="title" header="Title"></Column>
-            <Column field="category" header="Category"></Column>
-            <Column field="description" header="description"></Column>
-            <Column field="date" header="Date"></Column>
-            <Column field="time" header="Time"></Column>
-            <Column field="organizer" header="Organizer"></Column>
+            <Column field="title" header="Title" :sortable="true"></Column>
+            <Column field="category" header="Category" :sortable="true"></Column>
+            <Column field="description" header="description" :sortable="true"></Column>
+            <Column field="date" header="Date" :sortable="true"></Column>
+            <Column field="time" header="Time" :sortable="true"></Column>
+            <Column field="organizer" header="Organizer" :sortable="true"></Column>
             <template #paginatorLeft>
                 <Button type="button" icon="pi pi-refresh" class="p-button-text" />
             </template>
@@ -63,7 +70,11 @@ methods: {
             this.eventFilters = {
                 'global': {value: null, matchMode: FilterMatchMode.CONTAINS}
             }
-        }  
+        } ,
+        // export data
+        exportCSV() {
+            this.$refs.dt.exportCSV();
+        } 
 },
 }
 </script>
